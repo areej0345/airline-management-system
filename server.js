d876de8d40ebf97a18929e8eba2c2badbfbaf4ea
aree668 <areejalimustafa@gmail.com>
2026-04-21 01:51:02 +0500

    fixed cors syntax

--- server.js
+++ server.js
@@ -4,15 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-// Middleware
-
-
-app.use(cors({
-  origin: '*',
-  methods: ['GET','POST','PUT','DELETE'],
-  allowedHeaders: ['Content-Type']
-}));
+app.use(cors());
+app.use(express.json());
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/airlineDB')
   .then(() => {
