5f556d0ffda7d7ad01bba73c17d4880996313433
aree668 <areejalimustafa@gmail.com>
2026-04-20 14:07:59 +0500

    Fix CORS

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/airlineDB')
   .then(() => {
