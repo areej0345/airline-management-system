7faf1ae4b9b95148246d5d107fff4d607bda5538
aree668 <areejalimustafa@gmail.com>
2026-04-20 09:08:59 +0500

    Add vercel config

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'https://airline-management-system-production-ea33.up.railway.app/api';
+const BASE_URL = 'http://localhost:5000/api';
+//const BASE_URL = 'https://airline-management-system-production-ea33.up.railway.app/api';
 
 // ==================
 // FLIGHTS API
