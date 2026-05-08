957c763777b2081ac545532980335989c75a7169
aree668 <areejalimustafa@gmail.com>
2026-05-09 02:11:53 +0500

    update fix.js with regex replace

--- fix.js
+++ fix.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
-const path = 'node_modules/express/lib/application.js';
-let f = fs.readFileSync(path, 'utf8');
-f = f.replace("this.options('*', cb)", "this.options('/{*splat}', cb)");
-fs.writeFileSync(path, f);
-console.log('Fixed!');
-console.log('Fixed!');
+const filePath = 'node_modules/express/lib/application.js';
+let content = fs.readFileSync(filePath, 'utf8');
+
+console.log('Looking for options handler...');
+
+// Try multiple possible strings
+content = content
+  .replace(/this\.options\('\*'/g, "this.options('/{*splat}'")
+  .replace(/app\.options\('\*'/g, "app.options('/{*splat}'");
+
+fs.writeFileSync(filePath, content);
+console.log('Express patched successfully!');
