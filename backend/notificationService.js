1c3dd2af50320580f0e1dc941f8de172d23b7e8c
aree668 <areejalimustafa@gmail.com>
2026-05-15 14:22:59 +0500

    Fix Gmail SMTP

--- backend/notificationService.js
+++ backend/notificationService.js
@@ -2,23 +2,17 @@ const dns = require('dns');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  host: "smtp.gmail.com",
+  host: 'smtp.gmail.com',
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
   tls: {
-    family: 4,
     rejectUnauthorized: false
   }
 });
-
 async function sendEmail(to, subject, html) {
   try {
     await transporter.sendMail({
